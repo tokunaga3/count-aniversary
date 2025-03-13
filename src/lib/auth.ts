6b3e03aee4dc14ae43dc875a3d654726a736e054
tokunaga3 <mutsukitokunaga@outlook.jp>
2025-03-13 23:13:53 +0900

    secret設定

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,6 +11,7 @@ declare module "next-auth" {
   
 
 export const authOptions: NextAuthOptions = {
+    secret: process.env.NEXTAUTH_SECRET!,
     providers: [
       GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID!,
