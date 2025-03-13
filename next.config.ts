1fd72f83bf8358f2d6b6832e01b5a5c92a0d41c2
tokunaga3 <mutsukitokunaga@outlook.jp>
2025-03-14 00:18:11 +0900

    nextconfig設定

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  experimental: {
+    serverComponentsExternalPackages: ["googleapis"],
+  },
+};
+
 export default nextConfig;
